36cceef3118c8327b2261f810b9b774377ab3208
MD AL EMRAN <mdalemrananas@gmail.com>
2026-02-16 08:17:15 +0600

    Fix base path case to match repository name exactly

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: "/Portfolio-Md-Al-Emran/",
+  base: "/PORTFOLIO-WEBSITE-MD-AL-EMRAN/",
   server: {
     host: "::",
     port: 8080,
